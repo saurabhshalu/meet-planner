ca0ea7c26f93a11fd4d639ff7d2c0352f7d58b69
Saurabh Verma <saurabhshalu@gmail.com>
2023-10-08 11:11:10 +0530

    API Integration

--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -1,6 +1,41 @@
+import { useState } from "react";
 import LoginRegisterHeader from "../LoginRegisterHeader/LoginRegisterHeader.jsx";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const { data } = await axios.post(
+        "http://localhost:8000/auth/local/loginme",
+        {
+          email,
+          password,
+        }
+      );
+      if (!data.success) {
+        return toast.error(data.message || "Something went wrong....");
+      }
+
+      window.localStorage.setItem("user", JSON.stringify(data.message));
+      navigate("/Dashboard");
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  console.log(loading);
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -22,6 +57,8 @@ function Login() {
                   type="email"
                   autoComplete="email"
                   required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -51,6 +88,8 @@ function Login() {
                   type="password"
                   autoComplete="current-password"
                   required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -58,6 +97,7 @@ function Login() {
 
             <div>
               <button
+                onClick={loginHandler}
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
@@ -105,12 +145,12 @@ function Login() {
 
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?{" "}
-            <a
-              href="#"
+            <Link
+              to="/Register"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               Sign Up
-            </a>
+            </Link>
           </p>
         </div>
       </div>
--- frontend/src/OtpVerification/OtpVerification.jsx
+++ frontend/src/OtpVerification/OtpVerification.jsx
@@ -1,8 +1,44 @@
+import { useState } from "react";
 import LoginRegisterHeader from "../LoginRegisterHeader/LoginRegisterHeader.jsx";
+import { toast } from "react-toastify";
+import axios from "axios";
+import PropTypes from "prop-types";
+import FullScreenLoader from "../Components/FullScreenLoader.jsx";
+import { useNavigate } from "react-router-dom";
 
-function OtpVerification() {
+const OtpVerification = ({ email }) => {
+  const [otp, setOTP] = useState("");
+
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const verifyOTP = async (e) => {
+    e.preventDefault();
+    if (!otp.trim() || !email) {
+      return toast.error("Please enter OTP to proceed.");
+    }
+    setLoading(true);
+    try {
+      const { data } = await axios.post(
+        "http://localhost:8000/auth/local/otp/validate",
+        { otp, email }
+      );
+
+      if (!data.success) {
+        return toast.error(data.message || "Something went wrong....");
+      }
+
+      toast.success(data.message);
+      navigate("/", { replace: true, state: { newUser: true } });
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <>
+      {loading && <FullScreenLoader />}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <LoginRegisterHeader isWhat="OTP" />
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm space-y-6">
@@ -19,6 +55,8 @@ function OtpVerification() {
                 name="otp"
                 type="number"
                 required
+                value={otp}
+                onChange={(e) => setOTP(e.target.value)}
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -26,6 +64,7 @@ function OtpVerification() {
           <div>
             <button
               type="submit"
+              onClick={verifyOTP}
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Verify
@@ -35,6 +74,10 @@ function OtpVerification() {
       </div>
     </>
   );
-}
+};
+
+OtpVerification.propTypes = {
+  email: PropTypes.string.isRequired,
+};
 
 export default OtpVerification;
