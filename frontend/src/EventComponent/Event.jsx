a32aee24f267aec813ada9bb94bb0569524ea28c
Saurabh Verma <saurabhshalu@gmail.com>
2023-10-08 13:26:00 +0530

    list items

--- frontend/src/EventComponent/Event.jsx
+++ frontend/src/EventComponent/Event.jsx
@@ -1,42 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import EventList from "./EventList/EventList";
 import EventCalView from "./EventCalView/EventCalView";
 import { useNavigate } from "react-router-dom";
+import useAuthenticated from "../hooks/useAuthenticated";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export default function Events() {
   const [mode, setMode] = useState(true);
   const navigate = useNavigate();
 
+  const { userDetails } = useAuthenticated();
+
+  const [apiData, setAPIdata] = useState(null);
+
+  const [isGoogleLinked, setIsGoogleLinked] = useState(true);
+
+  useEffect(() => {
+    const callAPI = async () => {
+      try {
+        if (userDetails.email) {
+          const { data } = await axios.get(
+            `http://localhost:8000/events/${userDetails.email}`
+          );
+          if (!data.success) {
+            setIsGoogleLinked(data.isGoogleLinked);
+            return toast.error(data.message || "Something went wrong....");
+          }
+
+          setAPIdata(data);
+        }
+      } catch (error) {
+        toast.error(error?.response?.data?.message || error.message);
+      }
+    };
+    callAPI();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const listItems = apiData.success ? apiData.message : [];
+
   return (
     <>
       <main className="container mx-auto px-4 my-8">
-        <h1 className="text-xl text-bold ml-6">My Appointments</h1>
-        <nav className="flex flex-row">
-          <div
-            className={
-              "Upcoming-events p-6 border-b-1" +
-              (mode ? "border-black" : "border-transparent")
-            }
-            onClick={() => setMode(true)}
-          >
-            <h4 className={"text-bold text-md"}>Upcoming appointments</h4>
-          </div>
-          <div
-            className={
-              "Upcoming-events p-6 border-b-1" +
-              (mode ? "border-transparent" : "border-black")
-            }
-            onClick={() => setMode(false)}
-          >
-            <h4 className="text-bold text-md ">Calender View</h4>
-          </div>
-          <div
-            className={
-              "Upcoming-events p-6 border-b-1" +
-              (mode ? "border-transparent" : "border-black")
-            }
-            onClick={() => setMode(false)}
-          >
+        <div className="flex justify-between">
+          <h1 className="text-xl text-bold ml-6">My Appointments</h1>
+          <div className="flex gap-3 align-middle">
+            <span>Hello, {userDetails.displayName}</span>
             <button
               onClick={() => {
                 window.localStorage.removeItem("user");
@@ -47,9 +56,43 @@ export default function Events() {
               Logout
             </button>
           </div>
-        </nav>
+        </div>
+        {isGoogleLinked ? (
+          <>
+            <nav className="flex flex-row">
+              <div
+                className={
+                  "Upcoming-events p-6 border-b-1" +
+                  (mode ? "border-black" : "border-transparent")
+                }
+                onClick={() => setMode(true)}
+              >
+                <h4 className={"text-bold text-md"}>Upcoming appointments</h4>
+              </div>
+              <div
+                className={
+                  "Upcoming-events p-6 border-b-1" +
+                  (mode ? "border-transparent" : "border-black")
+                }
+                onClick={() => setMode(false)}
+              >
+                <h4 className="text-bold text-md ">Calender View</h4>
+              </div>
+            </nav>
 
-        {mode ? <EventList /> : <EventCalView />}
+            {mode ? <EventList /> : <EventCalView />}
+          </>
+        ) : (
+          <div>
+            <button
+              onClick={() => {
+                window.open("http://localhost:8000/auth/google", "_self");
+              }}
+            >
+              Link Google Account
+            </button>
+          </div>
+        )}
       </main>
     </>
   );
--- routes/events.js
+++ routes/events.js
@@ -13,6 +13,21 @@ router.get("/:uniqueId", async (req, res) => {
         .status(404)
         .json({ success: false, message: "No Events Found." });
     }
+    if (!myUser.isVerified) {
+      return res.status(200).json({
+        success: false,
+        isVerified: false,
+        message: "Please Verify the account to continue.",
+      });
+    }
+    if (!myUser.google.refresh_token) {
+      return res.status(200).json({
+        success: false,
+        isGoogleLinked: false,
+        isVerified: true,
+        message: "Google Account Not Linked, Please Link the Account First.",
+      });
+    }
     if (
       (myUser.google.expiry_date &&
         new Date(myUser.google.expiry_date) <= new Date()) ||
@@ -45,15 +60,34 @@ router.get("/:uniqueId", async (req, res) => {
       myUser.google.expiry_date = expiry_date;
       await myUser.save();
     }
-    const { data } = await axios.get(
-      `https://www.googleapis.com/calendar/v3/calendars/${myUser.email}/events`,
-      {
-        headers: {
-          Authorization: `Bearer ${myUser.google.access_token}`,
-        },
-      }
-    );
-    res.status(200).json(data);
+
+    // const MY_URL = `https://www.googleapis.com/calendar/v3/calendars/${myUser.email}/events`;
+    const startDate = new Date();
+    startDate.setHours(0, 0, 0, 0);
+
+    const endDate = new Date(startDate);
+    endDate.setMonth(endDate.getMonth() + 3);
+    endDate.setHours(23, 59, 59);
+
+    const MY_URL = `https://www.googleapis.com/calendar/v3/calendars/${
+      myUser.email
+    }/events?timeMin=${startDate.toISOString()}&timeMax=${endDate.toISOString()}`;
+
+    console.log(MY_URL);
+
+    const { data } = await axios.get(MY_URL, {
+      headers: {
+        Authorization: `Bearer ${myUser.google.access_token}`,
+      },
+    });
+    res.status(200).json({
+      success: true,
+      message: data?.items?.filter(
+        (item) =>
+          item.end.hasOwnProperty("dateTime") &&
+          item.start.hasOwnProperty("dateTime")
+      ),
+    });
   } catch (error) {
     console.log(error);
     return res
